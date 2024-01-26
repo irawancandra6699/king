7b2643e2f8948d40880b7527f30e94466581ed5a
irawancandra6699 <134005006+irawancandra6699@users.noreply.github.com>
2024-01-27 03:36:11 +0700

    Update king.sh

--- king.sh
+++ king.sh
@@ -633,7 +633,7 @@ fi
 clear
 echo "Banner /etc/banner.txt" >>/etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/banner.txt"@g' /etc/default/dropbear
-wget -O /etc/kyt.txt "${REPO}banner/issue.net"
+wget -O /etc/banner.txt "${REPO}banner/issue.net"
 print_success "Fail2ban"
 }
 function ins_epro(){
